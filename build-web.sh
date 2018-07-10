4681bfb9e6babe2b563331e41de1a7283b36ad4f
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-07-10 14:44:23 +0300

    Script for building web resources. Assumes that commonsos-web locates in parallel folder

---
+++ build-web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd ../commonsos-web
+npm run build-mobile
+popd
+
+echo "Copying web resources to cordova.."
+
+cp -r ~/work/commonsos-web/dist/* www/
+
+echo "Distributing web resources to supported platforms"
+
+cordova prepare
